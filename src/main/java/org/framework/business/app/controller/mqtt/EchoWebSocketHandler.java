a4275cd9dc8ac754bcfda7cb1bf06033cbaac451
jimyth01 <370851411@qq.com>
2016-10-18 07:18:14 +0800

    增加MQTT

--- src/main/java/org/framework/business/app/controller/mqtt/EchoWebSocketHandler.java
+++ src/main/java/org/framework/business/app/controller/mqtt/EchoWebSocketHandler.java
@@ -1,6 +1,9 @@
 package org.framework.business.app.controller.mqtt;
 
+import org.apache.activemq.util.ByteSequence;
+import org.framework.business.app.controller.mqtt.codec.MQTTWireFormat;
 import org.framework.business.app.service.EchoService;
+import org.fusesource.mqtt.codec.MQTTFrame;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.socket.BinaryMessage;
 import org.springframework.web.socket.TextMessage;
@@ -14,6 +17,7 @@ import org.springframework.web.socket.handler.AbstractWebSocketHandler;
 public class EchoWebSocketHandler extends AbstractWebSocketHandler {
 
 	private final EchoService echoService;
+	private MQTTWireFormat wireFormat = new MQTTWireFormat();
 
 
 	@Autowired
@@ -29,6 +33,7 @@ public class EchoWebSocketHandler extends AbstractWebSocketHandler {
 
 	@Override
 	protected void handleBinaryMessage(WebSocketSession session, BinaryMessage message) throws Exception {
+		MQTTFrame frame = (MQTTFrame)wireFormat.unmarshal(new ByteSequence(message.getPayload().array(), 0, message.getPayload().array().length));
 		session.sendMessage(message);
 	}
 }
